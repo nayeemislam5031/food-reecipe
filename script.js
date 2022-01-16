b6f96c9b276d2471529515c034699bd15ef61085
nayeemislam5031 <you@example.comnayeemislam5031@gmail.com>
2022-01-16 14:00:16 +0600

    before java script

---
+++ script.js
@@ -0,0 +1,8 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('meal-details-content');
+
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+//event listener
+searchBtn.addEventListener('click',getMealList);
+
