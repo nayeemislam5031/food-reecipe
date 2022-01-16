d3eb48b35789aff1a8dad284f7283f3e0abb620c
nayeemislam5031 <you@example.comnayeemislam5031@gmail.com>
2022-01-16 19:38:06 +0600

    almost done

--- script.js
+++ script.js
@@ -5,4 +5,51 @@ const mealDetailsContent = document.querySelector('meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 //event listener
 searchBtn.addEventListener('click',getMealList);
+mealList.addEventListener('click',getMealRecipe);
 
+
+
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(res => res.json())
+    .then(data=> {
+        let html = "";
+        if (data.meals) {
+            data.meals.forEach(meal => {
+                html+= `
+                <div class="meal-item" data-id="${meal.idMeal}">
+                        <div class="meal-img">
+                            <img src="${meal.strMealThumb} " alt="Food">
+                        </div>
+                        <div class="meal-name">
+                            <h3>"${meal.strMeal} " </h3>
+                            <a href="#" class="recipe-btn">Get Recipe</a>
+                        </div>
+                </div>
+                `;
+            });
+            mealList.classList.remove('notFound')
+            
+        }
+
+        else{
+            html = "Sorry, we don't find any meal!";
+            mealList.classList.add('notFound');
+
+        }
+
+    mealList.innerHTML = html;    
+    });
+}
+
+
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        
+    }
+}
